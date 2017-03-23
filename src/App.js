094d2c3e2f1a339cf3c050b83b606216dfe66342
Sam <thesamreaves@gmail.com>
2017-03-22 22:44:35 -0400

    Initial commit with basic React boilerplate and MobX dependencies

---
+++ src/App.js
@@ -0,0 +1,18 @@
+/**
+ * Learn MobX
+ * Entry File
+ * Written by Sam Reaves
+ * March 22, 2017
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+
+  return (
+    <div>Hello, Sam</div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
